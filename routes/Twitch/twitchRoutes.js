90809d507e670d4d6cbc9bcac3c1e2d384160b18
Geoff Kelly <geoffkellync@gmail.com>
2023-06-13 12:35:31 -0400

    fixed route

--- routes/Twitch/twitchRoutes.js
+++ routes/Twitch/twitchRoutes.js
@@ -36,7 +36,7 @@ router.get('/get-twitch-chat-settings', async (req, res) => {
 
 router.get('/get-stripe-id', async (req, res) => {
     try {
-        const twitch_login = req.body;
+        const twitch_login = req.params.twitch_login;
 
         console.log('⛔️ twitch_login: ', twitch_login) //!DEBUG
 
