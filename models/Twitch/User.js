762d24c803819526e604f09676c88d5615671c87
Geoff Kelly <geoffkellync@gmail.com>
2023-06-14 22:25:11 -0400

    fixed bug that would not update access token

--- models/Twitch/User.js
+++ models/Twitch/User.js
@@ -10,53 +10,58 @@ const db = mongo.db(process.env.MONGO_DB_NAME);
 //* Setting User to Database. 
 exports.setUserToDb = async (userData) => {
     try {
-        const collection = db.collection('app_users')
-
-        const userObject = await collection.findOne({ email: userData.email })
-
-        const defaultUserObj = {
-            unx_id: uuid(),
-            email: userData.email,
-            twitch_id: userData.twitch_id,
-            twitch_login: userData.twitch_login,
-            twitch_display_name: userData.twitch_display_name,
-            twitch_image: userData.twitch_image,
-            twitch_view_count: userData.twitch_view_count,
-            twitch_streamer_status: userData.twitch_streamer_status,
-            twitch_created_date: userData.twitch_created_date,
-            twitch_description: userData.twitch_description,
-            access_token: userData.access_token,
-            refresh_token: userData.refresh_token,
-            client_id: userData.client_id,
-            client_secret: userData.client_secret,
-            created_at: new Date(),
-            user_paid: false,
-            applicatoin_type: 'Twitch',
-            stripe_id: null,
-            metaData: {}
-        }
-
-
-        if (userObject) {
-            const updatedUserObj = { ...defaultUserObj, ...userObject }
-            await collection.updateOne({ unx_id: userObject.unx_id }, { $set: updatedUserObj })
-            return { user: updatedUserObj, isNew: false }
-        }
-
-        
-        await collection.insertOne(defaultUserObj)
-
-        return {user: defaultUserObj, isNew: true}
-        
+      const collection = db.collection('app_users');
+  
+      const userObject = await collection.findOne({ email: userData.email });
+  
+      const defaultUserObj = {
+        unx_id: uuid(),
+        email: userData.email,
+        twitch_id: userData.twitch_id,
+        twitch_login: userData.twitch_login,
+        twitch_display_name: userData.twitch_display_name,
+        twitch_image: userData.twitch_image,
+        twitch_view_count: userData.twitch_view_count,
+        twitch_streamer_status: userData.twitch_streamer_status,
+        twitch_created_date: userData.twitch_created_date,
+        twitch_description: userData.twitch_description,
+        access_token: userData.access_token,
+        refresh_token: userData.refresh_token,
+        client_id: userData.client_id,
+        client_secret: userData.client_secret,
+        created_at: new Date(),
+        user_paid: false,
+        applicatoin_type: 'Twitch',
+        stripe_id: null,
+        metaData: {},
+      };
+  
+      if (userObject) {
+        const updatedUserObj = {
+          ...defaultUserObj,
+          access_token: userObject.access_token,
+          refresh_token: userObject.refresh_token,
+        };
+        await collection.updateOne(
+          { unx_id: userObject.unx_id },
+          { $set: updatedUserObj }
+        );
+        return { user: updatedUserObj, isNew: false };
+      }
+  
+      await collection.insertOne(defaultUserObj);
+  
+      return { user: defaultUserObj, isNew: true };
     } catch (error) {
-        consoleLoging({
-            id: "ERROR",
-            user: 'Server',
-            script: 'models/Twitch/User.js',
-            info: 'Error setting user to DB ' + error
-        })
+      consoleLoging({
+        id: 'ERROR',
+        user: 'Server',
+        script: 'models/Twitch/User.js',
+        info: 'Error setting user to DB ' + error,
+      });
     }
-}
+  };
+  
 
 exports.setStripeCustomerId = async (customerId, unx_id) => {
     try {
