eaa16f936a0c7e3ac207a55eb735b5a9a329ca38
Geoff Kelly <geoffkellync@gmail.com>
2023-06-28 23:05:17 -0400

    updated yt route

--- routes/YouTube/authRouteYT.js
+++ routes/YouTube/authRouteYT.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const consoleLoging = require('../../helpers/consoleLoging');
 const router = express.Router();
-const authModel = require('../../models/YouTube/AuthYT')
+const AuthModel = require('../../models/YouTube/AuthYT')
+const YouTubeModel = require('../../models/YouTube/Youtube')
 
 
 
 router.get('/login-youtube', async (req, res) => {
     try {
-        const loginURL = await authModel.getYouTubeLoginURL()
+        const loginURL = await AuthModel.getYouTubeLoginURL()
 
         res.status(200).json(loginURL)
 
@@ -44,7 +45,7 @@ router.post('/get-login-data', async (req, res) => {
     try {
         const { accessToken } = req.body
 
-        const userData = await authModel.getGoogleUserData(accessToken)
+        const userData = await YouTubeModel.getGoogleUserData(accessToken)
 
         res.status(200).json(userData)
         
