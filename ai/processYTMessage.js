a8d87f5fe07a8ad16390462a248d6615d1089f50
Geoff Kelly <geoffkellync@gmail.com>
2023-07-03 10:15:40 -0400

    fixed type in message polling route

--- ai/processYTMessage.js
+++ ai/processYTMessage.js
@@ -1,36 +1,33 @@
 const { chat } = require('googleapis/build/src/apis/chat')
 const YouTubeModel = require('../models/YouTube/Youtube')
 const consoleLoging = require('../helpers/consoleLoging')
+const Queue = require('../models/Twitch/Queue')
 
 
 const startMessagePolling = async (accessToken, chatId) => {
     try {
         console.log('⛔️ Starting Messgae Polling!') //!REMOVE
-        const messageArray = []
+        let messageArray = []
         while(true){
             const messageData = await YouTubeModel.getLiveChatMessages(accessToken,chatId)
             messageData.items.forEach(item => {
                 const exists = messageArray.some(chat => item.snippet.id === chat.messageId)
 
-                if (exists){
-                    return
+                if (!exists){
+                    const newMessage = {
+                        messageId: item.snippet.id,
+                        user: item.authorDetails.displayName,
+                        message: item.snippet.textMessageDetails.messageText,
+                        isOwner: item.authorDetails.isChatOwner,
+                        isMod: item.authorDetails.isChatModerator, // Corrected here
+                        timestamp: item.snippet.publishedAt,
+                        sender_img: item.authorDetails.profileImageUrl,
+                        chatSponsor: item.authorDetails.isChatSponsor
+                    }
+                    messageArray.push(newMessage)
+                    processYTMessage(newMessage)
                 }
-                messageArray.push({
-                    messageId: item.snippet.id,
-                    user: item.authorDetails.displayName,
-                    message: item.snippet.textMessageDetails.messageText,
-                    isOwner: item.authorDetails.isChatOwner,
-                    isMod: item.authorDetails.isChatOwner,
-                    timestamp: item.snippet.publishedAt,
-                    sender_img: item.authorDetails.profileImageUrl,
-                    chatSponsor: item.authorDetails.isChatSponsor
-                })
             }) 
-
-            if(messageData){
-                processYTMessage(messageArray)
-                return
-            }   
 
         await new Promise(resolve => setTimeout(resolve, messageData.pollingIntervalMillis));
     }
@@ -46,6 +43,7 @@ const startMessagePolling = async (accessToken, chatId) => {
         return false
     }
 }
+
 
 const processYTMessage = async (message) => {
     try {
